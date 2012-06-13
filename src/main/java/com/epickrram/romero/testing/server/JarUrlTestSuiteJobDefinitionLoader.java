2801ac8e3cfbeda14d3308021b0f5177879ae1f9
mark.web.mail@gmail.com <mark.web.mail@gmail.com@1f432ec0-f544-96bc-655b-f34f8ea2b742>
2012-06-13 19:25:45 +0000

    grab test config properties from remote URL

--- src/main/java/com/epickrram/romero/testing/server/JarUrlTestSuiteJobDefinitionLoader.java
+++ src/main/java/com/epickrram/romero/testing/server/JarUrlTestSuiteJobDefinitionLoader.java
@@ -24,6 +24,7 @@ import com.epickrram.romero.core.JobDefinitionLoader;
 import com.epickrram.romero.util.UrlLoader;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -37,20 +38,25 @@ import static com.epickrram.romero.testing.common.TestSuiteIdentifier.toMapKey;
 public final class JarUrlTestSuiteJobDefinitionLoader implements JobDefinitionLoader<TestSuiteIdentifier, Properties>
 {
     public static final String URL_PATTERN_PROPERTY = "server.loader.definition.jar.url.pattern";
+    public static final String TEST_CONFIG_RESOURCE_PATTERN_PROPERTY = "test.config.resource.url.pattern";
 
     private final UrlLoader urlLoader;
+    private final UrlBuilder testConfigPropertiesResourceUrlBuilder;
     private final JobIdentifierUrlBuilder urlBuilder;
 
-    public JarUrlTestSuiteJobDefinitionLoader(final JobIdentifierUrlBuilder urlBuilder, final UrlLoader urlLoader)
+    public JarUrlTestSuiteJobDefinitionLoader(final JobIdentifierUrlBuilder urlBuilder, final UrlLoader urlLoader,
+                                              final UrlBuilder testConfigPropertiesResourceUrlBuilder)
     {
         this.urlBuilder = urlBuilder;
         this.urlLoader = urlLoader;
+        this.testConfigPropertiesResourceUrlBuilder = testConfigPropertiesResourceUrlBuilder;
     }
 
     @Override
     public List<JobDefinition<TestSuiteIdentifier, Properties>> loadJobDefinitions(final String identifier)
     {
         final List<JobDefinition<TestSuiteIdentifier, Properties>> definitionList = new ArrayList<>();
+        final Properties testConfigProperties = getGeneralTestConfigProperties(identifier);
         final String url = urlBuilder.getUrl(identifier);
         try
         {
@@ -68,6 +74,7 @@ public final class JarUrlTestSuiteJobDefinitionLoader implements JobDefinitionLo
                 if(name.contains(".class"))
                 {
                     final Properties properties = new Properties();
+                    properties.putAll(testConfigProperties);
                     properties.setProperty(TestPropertyKeys.CLASSPATH_URL_PREFIX + ".tests", url);
                     final JobDefinition<TestSuiteIdentifier, Properties> definition =
                             new JobDefinitionImpl<>(toMapKey(toClassName(name)), properties);
@@ -78,6 +85,28 @@ public final class JarUrlTestSuiteJobDefinitionLoader implements JobDefinitionLo
             jarFile.close();
             download.delete();
             return definitionList;
+        }
+        catch(IOException e)
+        {
+            throw new RuntimeException("Unable to open url: " + url, e);
+        }
+    }
+
+    private Properties getGeneralTestConfigProperties(final String identifier)
+    {
+        final String url = testConfigPropertiesResourceUrlBuilder.getUrl(identifier);
+        try
+        {
+            final File download = urlLoader.downloadUrl(url, true);
+            if(download == null)
+            {
+                throw new RuntimeException("Unable to open url: " + url + ", not found");
+            }
+            final Properties properties = new Properties();
+            properties.load(new FileReader(download));
+
+            download.delete();
+            return properties;
         }
         catch(IOException e)
         {
